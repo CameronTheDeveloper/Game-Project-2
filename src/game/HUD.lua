c9cc276571899eb6b0c5c08080c937257c5b109b
Rat <58471843+RatTaco@users.noreply.github.com>
2025-04-24 22:46:28 -0700

    Format time

--- src/game/HUD.lua
+++ src/game/HUD.lua
@@ -7,6 +7,13 @@ function HUD:init(player)
     self.player = player
 end
 
+local function formatTime(seconds)
+    local mins = math.floor(seconds / 60)
+    local sec = math.floor(seconds % 60)
+
+    return string.format("%02d:%02d", mins, sec)
+end
+
 
 function HUD:update(dt)
     
@@ -15,8 +22,9 @@ end
 
 
 function HUD:draw()
+    local formattedTime = formatTime(self.player.timeElapsed)
     love.graphics.printf("Score:"..math.floor(self.player.score), hudFont, 10, 4, 100, "left")
-    love.graphics.printf("Time:"..math.floor(self.player.timeElapsed), hudFont, 0, 4, gameWidth, "center")
+    love.graphics.printf("Time:"..formattedTime, hudFont, 0, 4, gameWidth, "center")
 end
 
 return HUD

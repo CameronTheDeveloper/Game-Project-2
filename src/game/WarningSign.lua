9c3bf69c8911789cd48549c375ced963577881b5
Rat <58471843+RatTaco@users.noreply.github.com>
2025-04-25 02:02:59 -0700

    Add WarningSign tween update method

--- src/game/WarningSign.lua
+++ src/game/WarningSign.lua
@@ -4,11 +4,22 @@ local Tween = require "libs.tween"
 local warningSprite = require "/graphics/HUDandMENU/HUD/Warning_sign.png"
 local WarningSign = Class{}
 
-function WarningSign:Init(x, y)
+function WarningSign:init(x, y)
     self.x = x
     self.y = y 
+    self.alpha = 0
+    
+    self.tween = tween.new(1, self, {x = love.graphics.getWidth() - 100, alpha = 1}, "inOutQuad")
 end
 
+function WarningSign:update(dt)
+    if self.tween then
+        local complete = self.tween:update(dt)
+        if complete then
+            self.tween = nil
+        end
+    end
+end
 
 
 return WarningSign

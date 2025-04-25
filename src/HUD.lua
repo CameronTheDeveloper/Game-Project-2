8315835b23af2b33ce5d6315642d9d6251dc7709
Rat <58471843+RatTaco@users.noreply.github.com>
2025-04-24 19:57:09 -0700

    Add HUD.lua file

---
+++ src/HUD.lua
@@ -0,0 +1,21 @@
+local Class = require "libs.hump.class"
+
+-- local hudFont = love.graphics.newFont("fonts/Abaddon Bold.ttf",16)
+
+local HUD = Class{}
+function HUD:init(player)
+    self.player = player
+end
+
+
+function HUD:update(dt)
+    
+end
+
+
+
+function HUD:draw()
+
+end
+
+return HUD
